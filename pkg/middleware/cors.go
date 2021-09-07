508df0736d483c9932ab5578ae6712087a2820cf
julioc98 <julioc.santos98@gmail.com>
2021-09-07 19:49:03 -0300

    remove OPTIONS

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -10,12 +10,12 @@ import (
 func Cors(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	log.Println("Cors middleware")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	// w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-	// w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type")
 
-	if r.Method == "OPTIONS" {
-		return
-	}
+	// if r.Method == "OPTIONS" {
+	// 	return
+	// }
 
 	next(w, r)
 }
