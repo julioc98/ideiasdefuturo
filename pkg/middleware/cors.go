dec89a8fb30fa6f1391aec52538e5d089ae8cf1a
julioc98 <julioc.santos98@gmail.com>
2021-09-07 19:22:33 -0300

    fix cors

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -8,9 +8,8 @@ import (
 // Cors adiciona os headers para suportar o CORS nos navegadores.
 func Cors(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type")
-	w.Header().Set("Content-Type", "application/json, application/x-www-form-urlencoded")
 
 	if r.Method == "OPTIONS" {
 		return
